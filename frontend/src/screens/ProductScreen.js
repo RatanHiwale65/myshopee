4ea438905cab913855568b9356b34880e122391e
Ratan Hiwale <ratanhiwale1@gmail.com>
2022-07-21 14:47:11 +0530

    added error message and loading

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,6 +9,9 @@ import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
 import Card from 'react-bootstrap/Card';
 import Rating from '../components/Rating';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from './utils';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -39,15 +42,15 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
@@ -101,16 +104,13 @@ function ProductScreen() {
                     </Col>
                   </Row>
                 </ListGroup.Item>
-                {product.countInStock >0 && (
+                {product.countInStock > 0 && (
                   <ListGroup.Item>
-                    <div className='d-grid'>
-                      <Button variant="primary">
-                        Add to Cart
-                      </Button>
+                    <div className="d-grid">
+                      <Button variant="primary">Add to Cart</Button>
                     </div>
                   </ListGroup.Item>
                 )}
-
               </ListGroup>
             </Card.Body>
           </Card>
